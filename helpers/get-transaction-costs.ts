a1c62a4c31e976f150e659061114496c14626699
dydent <dydent.github@outlook.com>
2023-03-31 12:04:24 +0200

    WIP: refactor payment transmitter tests

--- helpers/get-transaction-costs.ts
+++ helpers/get-transaction-costs.ts
@@ -4,8 +4,12 @@ export const getTransactionCosts = async (
   tx: ContractTransaction
 ): Promise<BigNumber> => {
   // calculate referral transaction costs
+  // calculate referral transaction costs
   const txReceipt = await tx.wait();
+  // gas used by the transaction
   const txGasUsed = await txReceipt.gasUsed;
+  // gas price
   const txEffectiveGasPrice = await txReceipt.effectiveGasPrice;
+  // return tx costs
   return txGasUsed.mul(txEffectiveGasPrice);
 };
--- test/referral-payment-transmitter/V2ReferralPaymentTransmitter.test.ts
+++ test/referral-payment-transmitter/V2ReferralPaymentTransmitter.test.ts
@@ -1,19 +1,31 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { ethConverter } from "../../helpers/unit-converters";
+import { etherUnitConverter } from "../../helpers/unit-converters";
 import { expect } from "chai";
 import { REWARD_AMOUNT_PROPORTION_ERROR } from "../../helpers/constants/error-strings";
 import { deployUpgradableReferralPaymentTransmitter } from "../../helpers/test-helpers/payment-transmitter-fixtures";
 import { ethers, upgrades } from "hardhat";
 import { V2ReferralPaymentTransmitterUpgradable } from "../../typechain-types";
+import { BigNumber } from "ethers";
+import { EtherUnits } from "../../types/ValidUnitTypes";
+
+// -----------------------------------------------------------------------------------------------
+// TEST CONFIG VALUES
+// -----------------------------------------------------------------------------------------------
+const TEST_PRECISION_DELTA = 0;
 
 const CONTRACT_NAME = "V2ReferralPaymentTransmitterUpgradable";
 
 // -----------------------------------------------------------------------------------------------
 // TEST DEFAULT VALUES
 // -----------------------------------------------------------------------------------------------
-const DEFAULT_PAYMENT_AMOUNT: number = 10;
+const DEFAULT_UNIT: EtherUnits = EtherUnits.Ether;
+const DEFAULT_PAYMENT_AMOUNT: BigNumber = etherUnitConverter[DEFAULT_UNIT](
+  BigNumber.from(10)
+);
 // must be smaller than payment amount
-const DEFAULT_REFERRAL_REWARD = 1;
+const DEFAULT_REFERRAL_REWARD: BigNumber = etherUnitConverter[DEFAULT_UNIT](
+  BigNumber.from(1)
+);
 
 describe(`Testing ${CONTRACT_NAME} Referral Contract`, async () => {
   // helper function to deploy the referral contract
@@ -35,16 +47,19 @@ describe(`Testing ${CONTRACT_NAME} Referral Contract`, async () => {
     it(`${CONTRACT_NAME} should deploy upgradable contract with correct params`, async () => {
       const { receiver, proxyContract } = await loadFixture(defaultFixture);
 
-      const contractReward = await proxyContract.referralReward();
-      const contractPaymentAmount = await proxyContract.paymentAmount();
-      const contractReceiverAddress = await proxyContract.receiver();
+      const contractReward: BigNumber = await proxyContract.referralReward();
+      const contractPaymentAmount: BigNumber =
+        await proxyContract.paymentAmount();
+      const contractReceiverAddress: string = await proxyContract.receiver();
 
       // assertions
-      expect(contractReward.toBigInt()).to.equal(
-        ethConverter(DEFAULT_REFERRAL_REWARD).toBigInt()
+      expect(contractReward).to.be.closeTo(
+        DEFAULT_REFERRAL_REWARD,
+        TEST_PRECISION_DELTA
       );
-      expect(contractPaymentAmount.toBigInt()).to.equal(
-        ethConverter(DEFAULT_PAYMENT_AMOUNT).toBigInt()
+      expect(contractPaymentAmount).to.be.closeTo(
+        DEFAULT_PAYMENT_AMOUNT,
+        TEST_PRECISION_DELTA
       );
       expect(contractReceiverAddress).to.equal(receiver.address);
     });
@@ -52,10 +67,11 @@ describe(`Testing ${CONTRACT_NAME} Referral Contract`, async () => {
     it(`${CONTRACT_NAME} should throw if deployed with incorrect params`, async () => {
       const [receiver] = await ethers.getSigners();
 
-      const paymentAmountParam = ethConverter(DEFAULT_PAYMENT_AMOUNT);
-      const incorrectReferralRewardParam = ethConverter(
-        DEFAULT_PAYMENT_AMOUNT + 1
-      );
+      const paymentAmountParam: BigNumber = DEFAULT_PAYMENT_AMOUNT;
+      const incorrectReferralRewardParam: BigNumber =
+        DEFAULT_PAYMENT_AMOUNT.add(
+          etherUnitConverter[DEFAULT_UNIT](BigNumber.from(1))
+        );
 
       const referralContract = await ethers.getContractFactory(CONTRACT_NAME);
 
