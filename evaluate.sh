ac44e47c9f3c7d41a00e051feb0d5984c22f98b1
dydent <dydent.github@outlook.com>
2023-05-02 02:41:45 +0200

    add result data

--- evaluate.sh
+++ evaluate.sh
@@ -5,7 +5,7 @@
 #./evaluate.sh
 
 # define the different number of accounts the evaluation scripts should be run with
-evaluation_accounts_values=(10 100 1000)
+evaluation_accounts_values=(10 100 500)
 use_evaluation_accounts="${1:-true}"
 
 
